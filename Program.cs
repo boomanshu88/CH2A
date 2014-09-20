702d0ecb65c1317fc53960cd8cd7da8c774897df
Bradley Shumard <bradleyshumard@outlook.com>
2014-09-20 14:01:09 -0700

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ch2a
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            uint myNum;
+            myNum = 400000;
+            Console.WriteLine("Your lucky number is {0}!", myNum);
+            Console.ReadLine();
+
+        }
+    }
+}
